a6a4971d4bdd8f96b87927143b103ce21fa3dac8
Dcrezztopia <pascalis.sandi09@gmail.com>
2024-10-23 18:42:28 +0700

    Tugas Praktikum 2 unfinished

--- belanja/lib/models/item.dart
+++ belanja/lib/models/item.dart
@@ -1,6 +1,15 @@
 class Item {
   String name;
   int price;
+  String imgproduct;
+  int stok;
+  double rating;
 
-  Item({required this.name, required this.price});
+  Item({
+    required this.name,
+    required this.price,
+    required this.imgproduct,
+    required this.stok,
+    required this.rating,
+  });
 }
--- belanja/lib/pages/item-pages.dart
+++ belanja/lib/pages/item-pages.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class ItemPage extends StatelessWidget {
+   const ItemPage({Key? key}) : super(key: key);
+   
   @override
   Widget build(BuildContext context) {
+    final Item item = ModalRoute.of(context)!.settings.arguments as Item;
     // TODO: implement build
-    // final Item item = ModalRoute.of(context)!.settings.arguments as Item;
 
     return Scaffold(
       appBar: AppBar(
